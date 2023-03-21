34b5ef01201cf62bdb4c770c7449bd710fc4b353
diana1m <diana.maliarchuk.work@gmail.com>
2023-03-21 22:17:53 +0000

    Deploying to gh-pages from @ diana1m/goit-react-hw-05-movies@0159df26df032a1482d12b77c8bce281396fc485 🚀

---
+++ static/js/764.ad3436b3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[764],{764:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("p",{children:"Movies"})}}}]);
+//# sourceMappingURL=764.ad3436b3.chunk.js.map
